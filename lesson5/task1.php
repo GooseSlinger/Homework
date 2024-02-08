1b3b4c83a6941c70fb23d9290bab35d2b40874c0
Artem.U <uchevatkin.a@tonar.info>
2024-02-08 15:18:57 +0300

    complete homework

--- lesson5/task1.php
+++ lesson5/task1.php
@@ -1,14 +1,48 @@
 <?php
 
-    echo 'Введите год в котором вы хотите пойти на работу: ' . PHP_EOL;
-    $years = readline();
-    echo 'Введите месяц в году в котором вы хотите пойти на работу: ' . PHP_EOL;
-    $month = readline();
-    function workDays(int $years, int $month)
-    {
-        return date("d-M-Y", mktime(0, 0, 0, $month, 1, $years));
-         
-    };
+function workDays(int $years, int $month): array
+{
+    $finalResult = [];
+    $prevWorkDay = 0;
+    $countDays = cal_days_in_month(CAL_GREGORIAN, $month, $years);
+    $works = '(Работа зовет)';
+    $chill = '(чилл)';
 
-    echo workDays($years, $month);
-?>
+    for ($i = 1; $i <= $countDays; $i++) {
+        $needDaysOfWeek = date('l', mktime(0, 0, 0, $month, $i, $years));
+        $needDate = date("d-M-Y", mktime(0, 0, 0, $month, $i, $years));
+
+        if (in_array($needDaysOfWeek, ['Saturday', 'Sunday'])) {
+            $workDate = "$needDaysOfWeek $needDate $chill";
+            $prevWorkDay = 0;
+            $finalResult[] = $workDate;
+            continue;
+        }
+
+        switch ($prevWorkDay) {
+            case 0:
+                $workDate = "$needDaysOfWeek $needDate $works";
+                $prevWorkDay++;
+                break;
+            case 1:
+                $workDate = "$needDaysOfWeek $needDate $chill";
+                $prevWorkDay++;
+                break;
+            case 2:
+                $workDate = "$needDaysOfWeek $needDate $chill";
+                $prevWorkDay = 0;
+                break;
+        }
+
+        $finalResult[] = $workDate;
+    }
+
+    return $finalResult;
+}
+
+echo 'Введите год в котором вы хотите пойти на работу: ' . PHP_EOL;
+$years = readline();
+echo 'Введите месяц в году в котором вы хотите пойти на работу: ' . PHP_EOL;
+$month = readline();
+
+var_dump(workDays($years, $month));
