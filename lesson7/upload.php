695dbe2a617526fa5243a838977d05eec7392ac0
goosesl1nger <artemuchevatkin@gmail.com>
2024-02-26 23:23:43 +0300

    add new remark for 7 homework

--- lesson7/upload.php
+++ lesson7/upload.php
@@ -1,22 +1,24 @@
 <?php
-    $fileName = $_POST['text'];
-    $mediaContent = $_FILES['content'];
-    $extension = pathinfo($mediaContent['name'], PATHINFO_EXTENSION);
-
-    if (empty($fileName) || (isset($mediaContent) && $mediaContent['error'] !== UPLOAD_ERR_OK)) {
+    if(isset($_POST['text']) && isset($_FILES['content'])) {
+        $fileName = $_POST['text'];
+        $mediaContent = $_FILES['content'];
+    
+        if (empty($fileName) || $mediaContent['error'] !== UPLOAD_ERR_OK) {
+            header('Location: index.php');
+            exit;
+        } else {
+            $fileSize = $mediaContent['size'];
+            $finalPath = 'upload/' . $fileName . '/';
+            
+            if (!file_exists($finalPath)) {
+                mkdir($finalPath, 0777, true);
+            }
+            
+            move_uploaded_file($mediaContent['tmp_name'], $finalPath . $fileName . '.png');
+            
+            echo "Файл успешно загружен и находится в директории \"$finalPath\" и весит $fileSize байт. <br>";
+        }
+    } else {
         header('Location: index.php');
         exit;
-    } else {
-        $fileSize = $mediaContent['size'];
-        $folderSave = 'upload/';
-        $createFolderForFile = $fileName . '/';
-        $finalPath = $folderSave . $createFolderForFile;
-
-        if (!file_exists($finalPath)) {
-            mkdir($finalPath, 0777, true);
-        }
-
-        move_uploaded_file($mediaContent['tmp_name'], $finalPath . '/' . $fileName . '.png');
-
-        echo "Файл успешно загружен и находится в директории \"$finalPath\" и весит $fileSize байт. <br>";
-    }
+    } 
