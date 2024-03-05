8f5b79b9d4c24666d04d46b4ef54b31d1c86bee6
Artem.U <uchevatkin.a@tonar.info>
2024-03-05 16:47:38 +0300

    new homework start

---
+++ lesson10/ajax.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $("form").submit(function(event) {
+        var formData = {
+            user_name: $("#user_name").val(),
+            user_last_name: $("#user_last_name").val(),
+            user_second_name: $("#user_second_name").val(),
+        };
+
+        $.ajax({
+            type: "POST",
+            url: "dadata.php",
+            data: formData,
+            dataType: "json",
+            encode: true,
+        }).done(function(result) {
+            console.log(result);
+            console.log(user_second_name.val);
+            for (let value of result) {
+
+                $("#result").html(
+                    '<p>Полное имя ' + value.result + '</p><p>В лице ' + value.result_genitive + '</p><p>Кому ' + value.result_dative + '</p><p>Согласовано ' + value.result_ablative + '</p>'
+                );
+            }
+
+        });
+
+        event.preventDefault();
+    });
+});
